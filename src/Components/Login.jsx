1a46169dcfa6adff40ca2810dd2e4fd1f3b29522
mariavitoria <vitoriagaara@gmail.com>
2025-09-04 15:33:05 -0300

    login quase finalizado

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,22 +1,47 @@
 import "./Login.css"
+import { useState } from "react";
 const Login = () => {
+
+    const [matricula, setMatricula] = useState("");
+    const [senha, setSenha] = useState("");
+    const [erro, setErro] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!matricula || !senha) {
+            setErro("Preencha todos os campos.");
+            return;
+        }
+
+        if (senha.length < 6) {
+            setErro("A senha deve ter pelo menos 6 caracteres");
+            return;
+        }
+
+        console.log("Matrícula:", matricula, "Senha:", senha);
+        setErro("");
+    }
+
     return (
         <>
-            <form className="container-login">
+            <form className="container-login" onSubmit={handleSubmit}>
                 <div className="caixa-login-1">
                     <img className="logo-login" alt="logo-da-escola-faculdade" />
-                    <p className="descricao-login"></p>
+                    <p className="descricao-login">Bem vindo aos serviços digitais da Universidade IntelliPort</p>
 
                     <div className="input-container">
-                        <input className="input-login" type="text" id="matricula" placeholder=" " />
+                        <input className="input-login" type="text" id="matricula" placeholder=" " value={matricula} onChange={(e) => setMatricula(e.target.value)} />
                         <label className="label-login" htmlFor="matricula">Matrícula</label>
-                        <div />
 
                     </div>
                     <div className="input-container">
-                        <input className="input-login" type="password" id="senha" placeholder=" " />
+                        <input className="input-login" type="password" id="senha" placeholder=" " value={senha} onChange={(e) => setSenha(e.target.value)} />
                         <label className="label-login" htmlFor="senha">Senha</label>
                         <h6 className="esqueceu-senha">Esqueceu a senha?</h6>
+
+                        {erro && <p className="erro-login">{erro}</p>}
+
                     </div>
 
                     <button className="botao-login" type="submit">Acessar</button>
