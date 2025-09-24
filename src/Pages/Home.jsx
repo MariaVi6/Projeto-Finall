2d9b49dc8b48c91c9ffaf7406b9a58b2df6e3728
mariavitoria <vitoriagaara@gmail.com>
2025-09-24 20:24:54 -0300

    testa ai

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,12 @@
 import Header from '../Components/Header'
 import CorpoHome from '../Components/CorpoHome'
 import Footer from '../Components/Footer'
-import Perfil from '../Components/Perfil'
 const Home = () => {
   return (
     <>
       <Header />
       <CorpoHome />
       <Footer />
-      <Perfil/>
     </>
   )
 }
